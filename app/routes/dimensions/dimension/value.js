c3f4430e010fbcc8a5ce64cf18930ff8211472f0
Brandon Williams <williamsbdev@gmail.com>
2016-12-31 21:30:00 -0600

    found the parent route params (kinda hidden)

--- app/routes/dimensions/dimension/value.js
+++ app/routes/dimensions/dimension/value.js
@@ -1,8 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-    model: function() {
+    model: function(params, transition) {
         console.log("Value route");
+        console.log(params);
+        console.log(transition);
+        console.log(transition.params["dimensions.dimension"].dimension_id);
+        console.log(transition.resolvedModels["dimensions.dimension"].dimension_id);
         console.log("Params for dimension: ");
         console.log(this.paramsFor("dimension"));
         console.log(this.paramsFor("dimension").dimension_id);
