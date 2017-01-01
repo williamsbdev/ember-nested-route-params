59143bef79ab6b8eb72177daad38f0a24bb0d61e
Brandon Williams <williamsbdev@gmail.com>
2016-12-31 21:38:14 -0600

    remove resolvedModels as that was silly hack that I did that worked

--- app/routes/dimensions/dimension/value.js
+++ app/routes/dimensions/dimension/value.js
@@ -6,7 +6,6 @@ export default Ember.Route.extend({
         console.log(params);
         console.log(transition);
         console.log(transition.params["dimensions.dimension"].dimension_id);
-        console.log(transition.resolvedModels["dimensions.dimension"].dimension_id);
         console.log("Params for dimension: ");
         console.log(this.paramsFor("dimension"));
         console.log(this.paramsFor("dimension").dimension_id);
